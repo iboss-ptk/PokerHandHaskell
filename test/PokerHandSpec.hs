6fdaa897015c2fafdb01e08972fff1c2dde8fee7
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-08-24 16:23:44 +0700

    Improve test descriptions

--- test/PokerHandSpec.hs
+++ test/PokerHandSpec.hs
@@ -7,22 +7,22 @@ spec :: Spec
 spec =
   describe "PokerHand" $
     describe "judge" $ do
-      context "first player win" $ do
-        it "wins with high card and named Black" $
+      context "when first player win" $ do
+        it "annouces first player as winner: Black with high card 9" $
           judge "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH" `shouldBe` "Black wins. - with high card: 9"
 
-        it "wins with full house and named John" $
+        it "annouces first player as winner: John with fullhouse 4 over 2" $
           judge "John: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S" `shouldBe` "John wins. - with full house: 4 over 2"
 
       context "second player wins" $
-        it "wins with high card and named White" $
+        it "annouces first player as winner: White with high card Ace" $
           judge "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH" `shouldBe` "White wins. - with high card: Ace"
 
-      context "ties" $
-        it "ties" $
+      context "when it's a tie" $
+        it "annouces tie" $
           judge "Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH" `shouldBe` "Tie."
 
       context "when input is invalid" $
-        it "tells user that input is not valid" $
+        it "announce that input is not valid" $
           judge "random input" `shouldBe` "`random input` is not a valid input."
 
