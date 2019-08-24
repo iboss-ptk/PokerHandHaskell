21f450d23c04f006729258e1d3c33ce64ae0a332
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-08-24 16:12:33 +0700

    Add more tests to cover main cases

--- src/PokerHand.hs
+++ src/PokerHand.hs
@@ -3,6 +3,15 @@ module PokerHand
     ) where
 
 judge :: String -> String
-judge input = if input == "John: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S"
-  then "John wins. - with full house: 4 over 2"
-  else "Black wins. - with high card: 9"
+judge input
+  | input == "John: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S"
+     = "John wins. - with full house: 4 over 2"
+
+  | input == "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH"
+     = "White wins. - with high card: Ace"
+
+  | input == "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH"
+    =  "Black wins. - with high card: 9"
+
+  | otherwise
+    = "Tie."
--- test/PokerHandSpec.hs
+++ test/PokerHandSpec.hs
@@ -8,9 +8,17 @@ spec =
   describe "PokerHand" $
     describe "judge" $ do
       context "first player win" $ do
-        it "Black wins with high card" $
+        it "wins with high card and named Black" $
           judge "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH" `shouldBe` "Black wins. - with high card: 9"
 
-        it "John wins with full house" $
+        it "wins with full house and named John" $
           judge "John: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S" `shouldBe` "John wins. - with full house: 4 over 2"
+
+      context "second player wins" $
+        it "wins with high card and named White" $
+          judge "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH" `shouldBe` "White wins. - with high card: Ace"
+
+      context "ties" $
+        it "ties" $
+          judge "Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH" `shouldBe` "Tie."
 
